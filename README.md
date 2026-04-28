4950d8785ffb86099ba0e75871ae51462b0675ce
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-04-28 05:57:28 +0000

    Add new pipelines `p1` and `p2` with initial dataset `q` and basic pipeline structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# nhbuyvky Repository
---
+++ pipelines/p1/code/p1/pipeline.py
@@ -0,0 +1,23 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from p1.config.ConfigStore import *
+from p1.functions import *
+from prophecy.utils import *
+from p1.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    df_q = q(spark)
+
+def main():
+    spark = SparkSession.builder.enableHiveSupport().appName("p1").getOrCreate()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/p1")
+    spark.conf.set("spark.default.parallelism", "4")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    registerUDFs(spark)
+    
+    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/p1", config = Config)(pipeline)
+
+if __name__ == "__main__":
+    main()
